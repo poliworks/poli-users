d33d12d2069911bfc9e4946e38f9d84f9043199c
Leonardo Iacovini <leoiacovini@gmail.com>
2016-12-10 18:06:45 -0200

    First commit

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject poli-users "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.1"]
+                 [ring/ring-json "0.4.0"]
+                 [com.datomic/datomic-free "0.9.5407"]
+                 [prismatic/schema "1.1.3"]
+                 [crypto-password "0.2.0"]
+                 [ring/ring-defaults "0.2.1"]
+                 [io.rkn/conformity "0.4.0"]
+                 [clj-jwt "0.1.1"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :resources-paths ["resources"]
+  :ring {:handler poli-users.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
